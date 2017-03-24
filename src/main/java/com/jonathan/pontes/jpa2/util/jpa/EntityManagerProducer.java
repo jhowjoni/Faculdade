fd94e91919972854837f1a93af22d12d1e41ebd4
Jonathan <jonathan.pontes@hotmail.com>
2017-03-23 22:42:28 -0300

    atualização para deletar especialidade

--- src/main/java/com/jonathan/pontes/jpa2/util/jpa/EntityManagerProducer.java
+++ src/main/java/com/jonathan/pontes/jpa2/util/jpa/EntityManagerProducer.java
@@ -4,7 +4,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -12,8 +11,6 @@ import javax.persistence.Persistence;
 @ApplicationScoped
 public class EntityManagerProducer {
 	
-	//@Inject
-    //private PersistenceProperties properties;
 	
 	private EntityManagerFactory factory;
 
